71a5a5fe80d03076f9248efbe671bf3b0ffc648a
Ben White <benjaminwhite02@gmail.com>
2026-02-11 13:25:08 +0000

    feature/default-tags

---
+++ terraform/layers/base/variables.tf
@@ -0,0 +1,17 @@
+variable "environment" {
+  description = "The environment to deploy the resources to"
+  type = string
+  default = "dev"
+}
+
+variable "project" {
+  description = "The project to deploy the resources to"
+  type = string
+  default = "terraformLab"
+}
+
+variable "createdBy" {
+  description = "The creator of the resources"
+  type = string
+  default = "Ben"
+}
